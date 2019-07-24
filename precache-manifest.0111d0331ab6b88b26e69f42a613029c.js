503c4b60e6b8297e1709c9114e104180c67b41c6
nikitsoni <47641941+nikitsoni@users.noreply.github.com>
2019-07-24 23:14:53 +0530

    Updates

---
+++ precache-manifest.0111d0331ab6b88b26e69f42a613029c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "908cf8da737a34af2551d431996c37f3",
+    "url": "/Recipe-App/index.html"
+  },
+  {
+    "revision": "f3ed68963841e96eba64",
+    "url": "/Recipe-App/static/css/main.8f0bbf34.chunk.css"
+  },
+  {
+    "revision": "573f771124ebfdec98dc",
+    "url": "/Recipe-App/static/js/2.35493de6.chunk.js"
+  },
+  {
+    "revision": "f3ed68963841e96eba64",
+    "url": "/Recipe-App/static/js/main.eafb4195.chunk.js"
+  },
+  {
+    "revision": "7c6d1fd2d3863083c7ca",
+    "url": "/Recipe-App/static/js/runtime~main.38a06a2c.js"
+  }
+]);
